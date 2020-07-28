afe567b24e98abc9228748e2c9561269a57a4efd
nimaabdpoor <nimaabdi328@yahoo.com>
2020-07-28 12:34:04 +0430

    function

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-//    val regex = Regex();
+    //    val regex = Regex();
 //    //containmatchin
 //    println(regex.containsmathin("A", "asdfaslfj"))
 //    println(regex.containsmathin("salam", "salam"))
@@ -12,10 +12,18 @@ fun main(args: Array<String>) {
 //    println(regex.mathces("n([iii+])ma","niiiiiiiiiiiiiiiimas"))
 //    println()
 
+    //prefer this one
+    fun max(a: Int, b: Int): Int {
+        return if (a < b) b else a
+    }
+
+    fun Block_Body_Max(a: Int, b: Int): Int = if (a > b) a else b
+    fun typeInferenceMax(a : Int, b : Int)  = if (a>b) a else b
 
 
-    val persons = listOf(person("alice",age = 20),person("nima",32),person("ahmad"))
-    val oldest = persons.maxBy { it.age ?:0 }
-    println("the oldest is : $oldest ")
+
+    println(max(2, 1))
+    println(Block_Body_Max(2, 1))
+    println(typeInferenceMax(2,1))
 }
-data class person(val name : String,val age : Int ?= null)
+
