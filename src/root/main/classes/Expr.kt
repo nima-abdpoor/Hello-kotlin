69aa5f9632d3e2b421eaa69b103af9569ff174f1
nimaabdpoor <nimaabdi328@yahoo.com>
2020-08-12 17:20:41 +0430

    usage of sealed modifier and restrict possibility of subclasses.

---
+++ src/root/main/classes/Expr.kt
@@ -0,0 +1,11 @@
+package root.main.classes
+
+sealed class Expr {
+    class Num(val value : Int) : Expr()
+    class Sum(val left: Expr,val right : Expr) : Expr()
+}
+fun eval(e  : Expr):Int =
+    when(e){
+        is Expr.Num -> e.value
+        is Expr.Sum -> eval(e.left) + eval(e.right)
+    }
